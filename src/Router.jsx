e0244279ec16eed0fbcee21cc34ff1bd52da77cd
elina <papadelina145@gmail.com>
2023-09-02 11:23:16 +0300

    Merge all api data in one array

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,26 +6,52 @@ import GamesPage from "./components/GamesPage";
 import useGameData from "./GameData";
 import { useState } from "react";
 
-
 const Router = () => {
+    let allGames = [];
+    const uniqueIds = [];
     const [cart, setCart] = useState([]);
     const {carouselData, newReleasesData, hotPicksData, bestSellingData, upcomingData} = useGameData();
+    if (carouselData) {
+        allGames = [...allGames, ...carouselData];
+    }
+    if (newReleasesData) {
+        allGames = [...allGames, ...newReleasesData];
+    }
+    if (hotPicksData) {
+        allGames = [...allGames, ...hotPicksData];
+    }
+    if (bestSellingData) {
+        allGames = [...allGames, ...bestSellingData];
+    }
+    if (upcomingData) {
+        allGames = [...allGames, ...upcomingData];
+    }
+
+    allGames = allGames.filter((game) => {
+        const isDuplicate = uniqueIds.includes(game.id);
+        
+        if(!isDuplicate) {
+            uniqueIds.push(game.id);
+            return true;
+        }
+        return false;
+    })
 
     const addItemToCart = (e) => {
         setCart(...cart, e.currentTarget.dataset.id)
     }
 
-
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Root cart={cart} setCart={setCart}/>,
+            element: <Root cart={cart} setCart={setCart} allGames={allGames}/>,
             errorElement: <ErrorPage />,
             children: [
                 {
                     errorElement: <ErrorPage />,
                     children: [
-                        {index: true, element: <Home addItemToCart={addItemToCart} 
+                        {index: true, element: <Home 
+                            addItemToCart={addItemToCart} 
                             carouselData={carouselData} 
                             newReleasesData={newReleasesData}
                             hotPicksData={hotPicksData}
