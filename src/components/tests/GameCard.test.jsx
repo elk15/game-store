666eb5918d9ebdc9a0c0ecb78680671d0819b1a4
elina <papadelina145@gmail.com>
2023-09-12 10:15:03 +0300

    Improve game card test

--- src/components/tests/GameCard.test.jsx
+++ src/components/tests/GameCard.test.jsx
@@ -1,19 +1,64 @@
 import GameCard from "../GameCard";
-import {  render, screen, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect } from "vitest";
-import { BrowserRouter, json } from 'react-router-dom';
+import { describe, expect} from "vitest";
 import { vi } from 'vitest'
+import { BrowserRouter } from "react-router-dom";
 
-it("adds items to cart", async () => {
-    const user = userEvent.setup();
-    const addItemToCart = vi.fn();
+
+describe("Game Card", () => {
+    it("calls addItemToCart once when the button is clicked", async () => {
+        const user = userEvent.setup();
+        const addItemToCart = vi.fn();
+
+        render(
+            <BrowserRouter>
+                <GameCard 
+                    addItemToCart={addItemToCart}
+                    image={null}
+                    title={"My Game"}
+                    id={0}
+                />
+            </BrowserRouter>
+        );
+        const addBtn = screen.getByRole('button', {name: 'add-to-cart'});
+        await user.click(addBtn);
     
-    render(
-        <GameCard addItemToCart={addItemToCart}/>
-    );
-    const addBtn = screen.getByLabelText('add-to-cart');
-    await user.click(addBtn);
+        expect(addItemToCart.mock.calls.length).toBe(1);
+    })
 
-    expect(addItemToCart).toHaveBeenCalled();
+    it("displays correct title", async () => {
+        const addItemToCart = vi.fn();
+        render(
+            <BrowserRouter>
+                <GameCard 
+                    addItemToCart={addItemToCart}
+                    image={null}
+                    title={"My Game"}
+                    id={0}
+                />
+            </BrowserRouter>
+        );
+        const gameTitle = screen.getByRole('heading');
+    
+        expect(gameTitle).toHaveTextContent("My Game")
+    })
+
+    it("shortens title if it's greater than 41 chars", async () => {
+        const addItemToCart = vi.fn();
+        render(
+            <BrowserRouter>
+                <GameCard 
+                    addItemToCart={addItemToCart}
+                    image={null}
+                    title={"Cthulhu Saves the World: Super Hyper Enhanced Championship Edition Alpha Diamond DX Plus"}
+                    id={0}
+                />
+            </BrowserRouter>
+        );
+        const gameTitle = screen.getByRole('heading');
+    
+        expect(gameTitle).toHaveTextContent("Cthulhu Saves the World: Super Hyper Enha")
+    })
 })
+
